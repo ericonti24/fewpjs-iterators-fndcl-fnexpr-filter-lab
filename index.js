6ec66d4a822309cd78f3d4cc6406426a3532ed20
Eric <ericontiveros123@gmail.com>
2022-01-14 16:30:06 -0700

    commit tree 862e18db20ea489eb2b9724adc971f9b447d2e4b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === str.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().indexOf(str.toLowerCase()) === 0;
+    });
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(function(driver) {
+        return driver.name === str;
+    });
+}
